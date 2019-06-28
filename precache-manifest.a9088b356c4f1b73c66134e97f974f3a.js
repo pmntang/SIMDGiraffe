3f0ef16a82508fb659079cb59c169545fe398642
Pierre Marie Ntang <ntang.pierre_marie@univ.teluq.ca>
2019-06-28 12:39:18 -0400

    Updates

---
+++ precache-manifest.a9088b356c4f1b73c66134e97f974f3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92c738e1b9d59c607f40",
+    "url": "/SIMDGiraffe/static/js/runtime~main.92c738e1.js"
+  },
+  {
+    "revision": "a4cc3886d974e6e646a8",
+    "url": "/SIMDGiraffe/static/js/main.a4cc3886.chunk.js"
+  },
+  {
+    "revision": "218e3729f084f38e1425",
+    "url": "/SIMDGiraffe/static/js/1.218e3729.chunk.js"
+  },
+  {
+    "revision": "a4cc3886d974e6e646a8",
+    "url": "/SIMDGiraffe/static/css/main.1cf061a6.chunk.css"
+  },
+  {
+    "revision": "218e3729f084f38e1425",
+    "url": "/SIMDGiraffe/static/css/1.35c818a1.chunk.css"
+  },
+  {
+    "revision": "29f10447fc42cc7ed48ce5eca8a31a50",
+    "url": "/SIMDGiraffe/index.html"
+  }
+];
