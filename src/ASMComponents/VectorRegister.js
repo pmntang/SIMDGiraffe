7f33cf6c8bacdca04ad776ab2af026def9811d1b
Pierre Marie Ntang <ntang.pierre_marie@univ.teluq.ca>
2019-07-04 21:32:29 -0400

    Semantic visualization of program

--- src/ASMComponents/VectorRegister.js
+++ src/ASMComponents/VectorRegister.js
@@ -151,6 +151,24 @@ function searchStep(lineIndex, columnIndex, aMatrix){
     return <tbody><tr>{ligne1}</tr><tr>{ligne2}</tr><tr>{ligne3}</tr></tbody>
      
   }
+  function initializeLinesMatrix(aLine, aMatrix){
+    var ligne1=<th rowspan="3" scope="rowgroup" className="empty"></th>, ligne2=null, ligne3=null;
+     for(let j=1; j<aMatrix[aLine].length; j++){
+      ligne1=<React.Fragment>{ligne1}<td className="empty"></td></React.Fragment>
+      ligne2=<React.Fragment>{ligne2}<td className="empty"></td></React.Fragment>
+      ligne3=<React.Fragment>{ligne3}<td className="empty"></td></React.Fragment>
+     }
+     return <tbody><tr>{ligne1}</tr><tr>{ligne2}</tr><tr>{ligne3}</tr></tbody>
+  }
+  function initializeFirstLineMatrix(firstLine, aMatrix){
+    var ligne1=<th rowspan="3" scope="rowgroup">{aMatrix[firstLine][0].name}</th>, ligne2=null, ligne3=null;
+     for(let j=1; j<aMatrix[firstLine].length; j++){
+      ligne1=<React.Fragment>{ligne1}<td className="empty"></td></React.Fragment>
+      ligne2=<React.Fragment>{ligne2}<td className="empty"></td></React.Fragment>
+      ligne3=<React.Fragment>{ligne3}<td className="empty"></td></React.Fragment>
+     }
+     return <tbody><tr>{ligne1}</tr><tr>{ligne2}</tr><tr>{ligne3}</tr></tbody>
+  }
 
   function retrieveIndexPositionLine(aLine, aColumn, aPosition,aMatrix){
     var statePos=null
@@ -223,8 +241,8 @@ class VectorRegister extends React.Component {
         this.registers=instructionsByRegisterBySteps(props.instructions)
         this.matrix=renameRegister(buildMatrixRegInt(this.registers, props.instructions))
         this.tableBodyInit=this.matrix.map((e,i)=>i==0?<tr>{e.map((x,j)=><th className="head">{x}</th>)}</tr>://initialization of a matrix (tableBodyInit)with empty cells except the first line which receives a cell with the name of registers.
-                                                 (i==1? e.map((x,j)=>j==0?<tr><th className="intrinsicName" rowspan="3" scope="rowgroup"><span className="intrinsicName">{this.matrix[1][0].name.toUpperCase()}</span></th><td ><span className="empty"></span></td><td ><span className="empty"></span></td><td ><span className="empty"></span></td></tr>:<tr><td ><span className="empty"></span></td><td ><span className="empty"></span></td><td ><span className="empty"></span></td></tr>):
-                                                       (e.map((x,j)=>j==0?<tr><th className="empty"   rowspan="3" scope="rowgroup"><span className="empty"></span></th><td ><span className="empty"></span></td><td ><span className="empty"></span></td><td ><span className="empty"></span></td></tr>:<tr><td ><span className="empty"></span></td><td ><span className="empty"></span></td><td ><span className="empty"></span></td></tr>))))
+                                                 (i==1? initializeFirstLineMatrix(i, this.matrix):initializeLinesMatrix(i, this.matrix)))//e.map((x,j)=>j==0?<tr><th className="intrinsicName" rowspan="3" scope="rowgroup"><span className="intrinsicName">{this.matrix[1][0].name.toUpperCase()}</span></th><td ><span className="empty"></span></td><td ><span className="empty"></span></td><td ><span className="empty"></span></td></tr>:<tr><td ><span className="empty"></span></td><td ><span className="empty"></span></td><td ><span className="empty"></span></td></tr>):
+                                                       //(e.map((x,j)=>j==0?<tr><th className="empty"   rowspan="3" scope="rowgroup"><span className="empty"></span></th><td ><span className="empty"></span></td><td ><span className="empty"></span></td><td ><span className="empty"></span></td></tr>:<tr><td ><span className="empty"></span></td><td ><span className="empty"></span></td><td ><span className="empty"></span></td></tr>))))
         this.state = {
             position: {line:1,column:0, rank:0, codeLine:this.matrix[1][0].line},
             tableBody:this.tableBodyInit
@@ -305,7 +323,7 @@ class VectorRegister extends React.Component {
             <div className="registerUsed"> 
                 <div className="controlButton">  </div>
                 <div className="visualization"><h6 className="text">Semantic visualization of the execution of the program {this.props.asm[0].name} <br/>Executed on {this.registers.length} registers in {this.props.instructions.length} instructions</h6>
-                <table className="visualization"><thead>{this.state.tableBody.map((e,i)=>i==0?<tr ref={i}>{e}</tr>:null)}</thead><tbody> {this.state.tableBody.map((e,i)=>i==0?null:<tr ref={i}>{e}</tr>)}</tbody></table>
+                <table className="visualization"><thead>{this.state.tableBody.map((e,i)=>i==0?e:null)}</thead> {this.state.tableBody.map((e,i)=>i==0?null:e)}</table>
                 </div>
                 <div className="presentation" className="text"><h6><strong>{this.props.description.find(x=>x.intrinsic.toLowerCase()==this.matrix[this.state.position.line][0].name).intrinsic} : {this.props.description.find(x=>x.intrinsic.toLowerCase()==this.matrix[this.state.position.line][0].name).description}</strong></h6>
               </div>
