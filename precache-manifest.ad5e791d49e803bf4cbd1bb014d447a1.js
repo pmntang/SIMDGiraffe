7704818e09288ddaaab5b8dbea39e91e1fff537e
Pierre Marie Ntang <ntang.pierre_marie@univ.teluq.ca>
2019-08-13 12:25:47 -0400

    Updates

---
+++ precache-manifest.ad5e791d49e803bf4cbd1bb014d447a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c25f2dba1c9f2b928caafe547769613",
+    "url": "/SIMDGiraffe/index.html"
+  },
+  {
+    "revision": "c85c6b816836f56d3e76",
+    "url": "/SIMDGiraffe/static/css/2.0fde4cac.chunk.css"
+  },
+  {
+    "revision": "3ba22306bd04eabbb9e7",
+    "url": "/SIMDGiraffe/static/css/main.9eb82b6c.chunk.css"
+  },
+  {
+    "revision": "c85c6b816836f56d3e76",
+    "url": "/SIMDGiraffe/static/js/2.b34e20dd.chunk.js"
+  },
+  {
+    "revision": "3ba22306bd04eabbb9e7",
+    "url": "/SIMDGiraffe/static/js/main.ddb76c17.chunk.js"
+  },
+  {
+    "revision": "3daab9fef607e40b1bfa",
+    "url": "/SIMDGiraffe/static/js/runtime~main.421c06c7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SIMDGiraffe/precache-manifest.4e0a5cff8d8777aaca85d0e696dc5744.js"
+  "/SIMDGiraffe/precache-manifest.ad5e791d49e803bf4cbd1bb014d447a1.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/SIMDGiraffe/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SIMDGiraffe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
