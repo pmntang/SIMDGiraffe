0cae405c8f660e19d7c7ebe98dfd4aec1840ecdc
pmntang <ntang.pierre_marie@univ.teluq.ca>
2020-07-13 13:30:50 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SIMDGiraffe/precache-manifest.acedf284f0efdafc3fea1c2dcd42abc5.js"
+  "/SIMDGiraffe/precache-manifest.8759a770320a207b4098b9b08897822e.js"
 );
 
 self.addEventListener('message', (event) => {
